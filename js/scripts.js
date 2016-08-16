a997997df4167bbc78baedb6ccfa7b4254f11fb3
Dean Scelza and Kyle Lange <dean@>
2016-08-16 12:02:14 -0700

    add more html structure and labels for javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
-// Buisiness Logic
+// Business Logic
+$(document).ready(function(){
+
+
 
 
 
 // User Interface Logic
+  $(form).submit(function(event){
+    event.preventDefault();
+    var userString = $(?????).val();
+
+
+
+
+  });
+});
